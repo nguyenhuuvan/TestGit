d64c36b4e4059001b7d0df00bfc5e61417e35e8d
Nguyễn Hữu Văn <vannh@lienvietech.com.vn>
2019-10-05 18:38:51 +0700

    Initial commit

---
+++ app/src/main/java/com/poly/testgit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.poly.testgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Master", Toast.LENGTH_SHORT).show();
+    }
+}
